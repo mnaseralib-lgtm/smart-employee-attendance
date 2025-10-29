8d109a1639631257f76fcc3d39f7ceca70772ab4
mnaseralib-lgtm <mnaseralib@gmail.com>
2025-10-29 17:03:03 +0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,16 +1,15 @@
+<script type="module">
+  import React from "https://esm.sh/react@18.2.0";
+  import ReactDOM from "https://esm.sh/react-dom@18.2.0/client";
+  import App from "./App.js";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+  const rootElement = document.getElementById("root");
+  if (!rootElement) {
+    throw new Error("Could not find root element to mount to");
+  }
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    React.createElement(React.StrictMode, null, React.createElement(App))
+  );
+</script>
